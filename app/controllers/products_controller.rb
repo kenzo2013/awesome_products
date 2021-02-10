18b650f631c2bb08edc4717443eb69f14291fc29
kenzo2013 <kkenzo2007@yahoo.fr>
2021-02-10 11:56:02 +0100

    Implement product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,35 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :new]
+  before_action :set_product, only: [:show]
   def index
-  end
-
-  def new
+    @products = Product.includes(:attachments)
   end
 
   def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product[:user_id] = current_user.id
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find_by(id: params[:id])
+  end
+  def product_params
+    params.require(:product).permit(:name, :description, files: [])
   end
 end
